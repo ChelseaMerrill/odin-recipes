de8bbb4f27ba0f2da68d205f63e6c751a15d860f
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-05-31 15:15:17 -0400

    finished rock paper scissors game

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -1,6 +1,63 @@
 let results = ['rock', 'paper', 'scissors']
-function computerPlay(res){
-   let ran = Math.floor(Math.random()*3)
-    return res[ran]
+winners =[];
+
+function game(){
+    for (let i = 1; i <= 5; i++) {
+        playRound(i);
+      }
+      document.querySelector("button").textContent = "Play new game";
 }
-console.log(computerPlay(results))
+
+function playRound(round) {
+    const playerSelection = playerChoice();
+    const computerSelection = computerPlay();
+    const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner);
+    logRound(playerSelection, computerSelection, winner, round);
+}
+
+function playerChoice() {
+    let input = prompt("Type Rock, Paper, or Scissors");
+    while (input == null) {
+      input = prompt("Type Rock, Paper, or Scissors");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    while (check == false) {
+      input = prompt(
+        "Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitilization doesnt matter"
+      );
+      while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+      }
+      input = input.toLowerCase();
+      check = validateInput(input);
+    }
+    return input;
+  }
+
+function computerPlay(){
+    return results[Math.floor(Math.random()*3)]
+    
+}
+
+function validateInput(res){
+    return results.includes(res);
+}
+
+function checkWinner(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return 'draw'
+    } else if ((playerSelection== 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || ( playerSelection=='scissors' && computerSelection == 'paper')){
+        return 'You win ' + playerSelection + ' beats ' + computerSelection + '!'
+    } else{
+        return 'You Lost ' + computerSelection + ' beats ' + playerSelection + '!'
+    }
+}
+
+  function logRound(playerChoice, computerPlay, winner, round) {
+    console.log("Round:", round);
+    console.log("Player Chose:", playerChoice);
+    console.log("Computer Chose:", computerPlay);
+    console.log(winner);
+  }
