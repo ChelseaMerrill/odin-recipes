ebffb4751428b71cfe0a4f748748ed3c44723676
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-06-01 10:40:25 -0400

    added reload

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -2,6 +2,10 @@ function menu(){
   document.getElementById('menu').innerHTML= 'To use this etch-a-sketch, click on each cell to fill it in'
 }
 
+function erase(){
+   location.reload()
+ }
+
 function fillCell(element, color){
    element.style.backgroundColor = color;
 }
