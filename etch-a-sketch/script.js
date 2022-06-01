218148f7aa1a9e05d3a67dc21b055f3421b607dc
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-06-01 10:11:48 -0400

    added instructions and onclick fill to boxes

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,3 +1,7 @@
 function menu(){
-   //create the popup
+  document.getElementById('menu').innerHTML= 'To use this etch-a-sketch, please select size of the grid you wish to use'
+}
+
+function fillCell(element, color){
+   element.style.backgroundColor = color;
 }
