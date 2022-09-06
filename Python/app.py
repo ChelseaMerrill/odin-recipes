55275045dbed1a738795fa90e4007bcbf88c3b7d
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-09-06 16:28:03 -0400

    todays lesson

--- Python/app.py
+++ Python/app.py
@@ -18,3 +18,132 @@ from operator import truediv
 # second = float(input('Second: '))
 # sum = first + second
 # print("Sum: " + str(sum))
+
+# course = 'Python for beginners'
+# print(course.upper())
+
+# course = 'Python for beginners'
+# print(course.find('y'))
+
+# course = 'Python for beginners'
+# print(course.find(' for'))
+
+# course = 'Python for beginners'
+# print(course.replace('for', '4'))
+
+# course = 'Python for beginners'
+# print("Python" in course)
+
+# print(10+3)
+
+# print(10 / 3)
+# print(10 // 3)
+# print(10 % 3)
+# print (10 ** 3)
+
+# x = 10 + 3 * 2
+# print(x)
+# y = (10 + 3) * 2
+# print(y)
+
+# x = 3 > 2
+# print(x)
+
+# x = 3 != 2
+# print(x)
+
+# price = 5
+# print (price > 10 and price < 30)
+
+# price = 5
+# print( not price > 10)
+
+# price = 5
+# print (price > 10 or price < 30)
+
+# temp = 5
+# if temp > 30 : 
+#     print ("it's a hot day")
+#     print ('drink lots of water')
+# print('not too hot')
+
+# temp = 5
+# if temp > 30 : 
+#     print ("it's a hot day")
+#     print ('drink lots of water')
+# elif temp > 20: 
+#     print("it's a nice day")
+# elif temp > 10 : 
+#     print("It's a bit cold")
+# else:
+#     print("it's freezing")
+
+# weight = float(input("Weight: "))
+# unit = input("(K)g or (L)bs: ")
+
+# if unit.upper() == "K" :
+#     converted = weight / 0.45
+#     print("Weight in Lbs: " + str(converted))
+# else:
+#     converted = weight * .45
+#     print("Weight in Kgs: " + str(converted))
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i = i +1
+
+# i = 1
+# while i <= 1_00:
+#     print(i)
+#     i = i +1
+
+# i = 1
+# while i <= 10:
+#     print(i * '*')
+#     i = i +1
+
+# names = ['chelsie', 'ashley', 'sam', 'dom', 'jared' ]
+# print(names[0])
+# print(names[1])
+# print(names[-1])
+
+# names[0] = 'chelsea'
+# print(names)
+
+# print(names[0:3])
+
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+# numbers.append(8) 
+#print(numbers) #[1, 2, 3, 4, 5, 6, 7, 8]
+
+#numbers.insert(0, -1) 
+#print(numbers) # [-1, 1, 2, 3, 4, 5, 6, 7, 8]
+
+#numbers.remove(3) 
+#print(numbers) #[-1, 1, 2, 4, 5, 6, 7, 8]
+
+#print(1 in numbers) #True
+
+#print(len(numbers)) #8
+
+#for item in numbers:
+    #print(item) 
+    # -1
+    # 1
+    # 2
+    # ...
+
+# new_numbers = range(5, 10, 2)
+# print(new_numbers)
+
+# for number in new_numbers:
+#     print(number)
+
+
+# for num in range (10):
+#     print(num)
+
+
+# numbers = (1, 2, 3)
+# print(numbers.count(3))
