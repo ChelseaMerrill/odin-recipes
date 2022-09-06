ba0f08c48705f1310d8e6a7665d85238d3122267
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-09-06 14:24:29 -0400

    basic addition

--- Python/app.py
+++ Python/app.py
@@ -1,8 +1,20 @@
 from operator import truediv
+# birth_year = input('Enter your birth year: ')
+# age = 2022 - int(birth_year)
+# print(age)
 
+# float() <--convert string or number to a floating num
+# int() <-- conver number or string into an interger
+# bool() <-- convert value to boolean
+# str() <--conver number to string
 
-age = 20
-price = 19.20
-is_online= True
-first_name='chelsea'
-print(age)
+# first = input('First: ')
+# second = input('Second: ')
+# sum = float(first) + float(second)
+# print("Sum: " + str(sum))
+
+# # Also Can Do:
+# first = float(input('First: '))
+# second = float(input('Second: '))
+# sum = first + second
+# print("Sum: " + str(sum))
