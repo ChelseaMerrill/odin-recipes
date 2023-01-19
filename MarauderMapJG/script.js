0b8cea6795080a89e6be5630b8d5d4ac3f1a65bb
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2023-01-19 17:24:56 -0500

    added more people to the canvas

--- MarauderMapJG/script.js
+++ MarauderMapJG/script.js
@@ -15,8 +15,12 @@ function myFunction() {
     var amanda_img = document.getElementById("amanda");
     var chelsea_img = document.getElementById("chelsea");
     var josh1_img = document.getElementById("josh1");
+    var eliza_img = document.getElementById("eliza");
+    var emily_img = document.getElementById("emily");
     ctx.drawImage(dom_img, 930, 450);
     ctx.drawImage(amanda_img, 10, 460);
     ctx.drawImage(chelsea_img, 50, 500);
     ctx.drawImage(josh1_img, 300, 720);
+    ctx.drawImage(eliza_img, 1100, 220);
+    ctx.drawImage(emily_img, 1060, 260);
 };
